aa431e7e45b37d5226586f10150c9b40562ca8b7
Alok Shankar <alok.shankar.m@gmail.com>
2015-08-20 23:27:57 +0530

    modular

--- evaluation1/clean.py
+++ evaluation1/clean.py
@@ -1,9 +1,15 @@
 import re
 
 def cleanup(text):
+	
 	text = input_file
 		.read()
 		.lower()
+
+	text = re.sub(r"#[^ \n\t]*", "token_hash", text)
+	text = re.sub(r"@[^ \n\t]*", "token_handle", text)
+	text = re.sub(r"(mailto\:|(news|(ht|f)tp(s?))\://)[^ \n\t]*" , "token_url", text)
+	text = re.sub(r"(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)", "token_emote", text)
 
 	emoji_regex = re.compile(u'['
 		u'\U0001F300-\U0001F64F'
@@ -13,10 +19,7 @@ def cleanup(text):
 	)
 
 	text = emoji_regex.sub('token_emoji', text)
-	text = re.sub(r"#[^ \n\t]*", "token_hash", text)
-	text = re.sub(r"@[^ \n\t]*", "token_handle", text)
-	text = re.sub(r"(mailto\:|(news|(ht|f)tp(s?))\://)[^ \n\t]*" , "token_url", text)
-	text = re.sub(r"(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)", "token_emote", text)
+
 	return text
 
 with open("C:\data\\input.txt", 'r') as input_file:
