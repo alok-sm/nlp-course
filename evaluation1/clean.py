1e4a58e7cc7495d0089274caac27215e5857b2cb
Alok Shankar <alok.shankar.m@gmail.com>
2015-08-20 23:25:52 +0530

    modular

--- evaluation1/clean.py
+++ evaluation1/clean.py
@@ -1,6 +1,6 @@
 import re
 
-with open("C:\data\\input.txt", 'r') as input_file:
+def cleanup(text):
 	text = input_file
 		.read()
 		.lower()
@@ -17,5 +17,9 @@ with open("C:\data\\input.txt", 'r') as input_file:
 	text = re.sub(r"@[^ \n\t]*", "token_handle", text)
 	text = re.sub(r"(mailto\:|(news|(ht|f)tp(s?))\://)[^ \n\t]*" , "token_url", text)
 	text = re.sub(r"(\:\w+\:|\<[\/\\]?3|[\(\)\\\D|\*\$][\-\^]?[\:\;\=]|[\:\;\=B8][\-\^]?[3DOPp\@\$\*\\\)\(\/\|])(?=\s|[\!\.\?]|$)", "token_emote", text)
+	return text
+
+with open("C:\data\\input.txt", 'r') as input_file:
+	text = cleanup(input_file.read())
 	with open("C:\data\\output.txt", "w") as output_file:
 		output_file.write(text)
