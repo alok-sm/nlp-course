1e9917a780dd55a70520c2f09298b145c4a2eac0
Alok Shankar <alok.shankar.m@gmail.com>
2015-08-20 23:23:05 +0530

    cleanup

--- evaluation1/clean.py
+++ evaluation1/clean.py
@@ -1,7 +1,18 @@
 import re
 
 with open("C:\data\\input.txt", 'r') as input_file:
-	text = input_file.read()
+	text = input_file
+		.read()
+		.lower()
+
+	emoji_regex = re.compile(u'['
+		u'\U0001F300-\U0001F64F'
+		u'\U0001F680-\U0001F6FF'
+		u'\u2600-\u26FF\u2700-\u27BF]+', 
+		re.UNICODE
+	)
+
+	text = emoji_regex.sub('token_emoji', text)
 	text = re.sub(r"#[^ \n\t]*", "token_hash", text)
 	text = re.sub(r"@[^ \n\t]*", "token_handle", text)
 	text = re.sub(r"(mailto\:|(news|(ht|f)tp(s?))\://)[^ \n\t]*" , "token_url", text)
