400c5353e618bff9ef6484f583bfe33ac9aba147
Alok Shankar <alok.shankar.m@gmail.com>
2015-08-29 12:49:48 +0530

    preprocess comments

--- eval1/Code/preprocess.py
+++ eval1/Code/preprocess.py
@@ -3,9 +3,9 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem import *
 import itertools
 
-def document_preprocess(doc):
+def document_preprocess(doc): # preprocess one document
 	# print doc
-	tokenizer = RegexpTokenizer(r"[a-zA-Z0-9']+")
+	tokenizer = RegexpTokenizer(r"[a-zA-Z0-9']+") # regex tokenizer. include single quote for 's'
 	stemmer = SnowballStemmer('english')
 	doc = remove_aliases(doc.lower())
 	# print doc
@@ -40,18 +40,18 @@ def document_preprocess(doc):
 		"it",
 		"it's"
 	]
-	doc = [stemmer.stem(word) for word in doc]
-	doc = [word for word in doc if word not in stopword_list]
+	doc = [stemmer.stem(word) for word in doc] #stem words
+	doc = [word for word in doc if word not in stopword_list] #remove stopwords
 
 	document = []
 	for word in doc:
 		document.append(word)
-		if(word == "four" or word == "six"):
-			document.append("boundary")
+		if(word == "four" or word == "six"): 
+			document.append("boundary") #if "four" or "six" replace by "four boundary" or "six boundary"
 	doc = [word[:-2] if word.endswith("'s") else word for word in document]
 	return doc
 
-def remove_aliases(comment):
+def remove_aliases(comment): #remove common aliases
 	aliases = {
 		"dot ball" : "0 run",
 		"no run" : "0 run",
@@ -65,7 +65,7 @@ def remove_aliases(comment):
 			comment.replace(alias, aliases[alias])
 	return comment
 
-def corpus_preprocess(comments):
+def corpus_preprocess(comments): #run document_preprocess on all docs in corpus
 	comments = [
 		document_preprocess(comment) for comment in comments
 	]
