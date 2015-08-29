d99eced5f2e3bc3e3b242770f274aabffcccd4a1
Akshay RD <HereticalDruid@users.noreply.github.com>
2015-08-29 12:55:29 +0530

    Update process.py
    process.py with comments

--- eval1/Code/process.py
+++ eval1/Code/process.py
@@ -1,8 +1,10 @@
 import nltk
 import math
 
+'''
+Function for generating Term frequency
+'''
 def tf(L):
-	#L = ast.literal_eval(document.read())
 	D = {}
 	for i in L:
 		if i not in D:
@@ -10,6 +12,9 @@ def tf(L):
 		D[i] += 1
 	return D
 
+'''
+Function for scoring without using idf and OR constraint
+'''
 def tf_or(query,docList):
 	tf = idf = 0
 	total = 0
@@ -30,9 +35,41 @@ def tf_or(query,docList):
 		L2.append(tf)
 	return topR(L2,10)
 
+'''
+Function for scoring without using idf and AND constraint
+'''
+def tfidf_and(query,docList):
+	tf = idf = 0
+	total = 0
+	L2 = []  #List of TFIDF
+	idfL = []
+	for w in query:
+		idfL.append(0)
+		for J in docList:
+			if w in J:
+				idfL[-1]+= 1
+
+	for J in docList:
+		tf=0
+		for w in range(len(query)):
+			if query[w] in J:
+				if not idfL[w] == len(docList):
+					tf += J[query[w]] * math.log(len(docList)/idfL[w])
+			else:
+				tf=0
+				break
+		L2.append(tf)
+	return topR(L2,10)
+
+'''
+Function to generate index of top 10 ranked documents
+'''
 def topR(L, n=10):
 	return [i for i in (sorted(range(len(L)), key=lambda k: L[k])[::-1][:10]) if not L[i] == 0]
 
+'''
+Function for scoring with tfidf and AND constraint
+'''
 def tf_and(query,docList):
 	tf = idf = 0
 	total = 0
@@ -56,6 +93,9 @@ def tf_and(query,docList):
 		L2.append(tf)
 	return topR(L2,10)
 
+'''
+Function for scoring with tfidf and OR constraint
+'''
 def tfidf_or(query,docList):
 	tf = idf = 0
 	total = 0
@@ -76,25 +116,3 @@ def tfidf_or(query,docList):
 		L2.append(tf)
 	return topR(L2,10)
 
-def tfidf_and(query,docList):
-	tf = idf = 0
-	total = 0
-	L2 = []  #List of TFIDF
-	idfL = []
-	for w in query:
-		idfL.append(0)
-		for J in docList:
-			if w in J:
-				idfL[-1]+= 1
-
-	for J in docList:
-		tf=0
-		for w in range(len(query)):
-			if query[w] in J:
-				if not idfL[w] == len(docList):
-					tf += J[query[w]] * math.log(len(docList)/idfL[w])
-			else:
-				tf=0
-				break
-		L2.append(tf)
-	return topR(L2,10)
