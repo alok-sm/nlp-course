dfd805976043d5e94fb620028f8a988d711f3475
Akshay RD <HereticalDruid@users.noreply.github.com>
2015-09-23 21:24:52 +0530

    Update tree_parse.py

--- lab2/code/tree_parse.py
+++ lab2/code/tree_parse.py
@@ -17,56 +17,92 @@ with open('data.txt', 'r') as f:
 
 def get_functions():
 
-	def is_first_word(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(index == 0)
+	def date1(history, tag):
+		word=history[2][history[3]][0]
+		r1=re.search("\d(\d)?[/-]\d(\d)?[/-]\d\d(\d\d)?",word)
+		r2=re.search("\d\d(\d\d)?[/-]\d(\d)?[/-]\d(\d)?",word)
+		if r1 and r1.group()==word or r2 and r2.group()==word and tag=="DATE":
+			return 1.0
+		return 0.0
 
-	def is_last_word(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(index + 1 == len(sentence))
+	def time1(history, tag):
+		word=history[2][history[3]][0]
+		r1=re.search("(\d)?\d:\d\d( *(a|p)\.?m\.?)",word)
+		r2=re.search("([0-1]\d[0-5]\d)|([2][0-3][0-5]\d)",word)
+		if r1 and r1.group()==word or r2 and r2.group()==word and tag=="TIME":
+			return 1.0
+		return 0.0
 
-	def capital_first_letter(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(sentence[index][0].isupper())
+	def isOther(history, tag):
+		word=history[2][history[3]][0]
+		pos=history[2][history[3]][1]
+		if (word[0].islower() or not re.search("NNP?",pos) or not is_number(history, tag)) and tag=="OTHER":
+			return 1.0
+		return 0.0
 
-	def follows_noun(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(tag_minus_1[0] == 'N')
+	def isPerson(history, tag):
+		if (history[3]>0 and re.search("NNP?", history[2][history[3]-1][1])) or (history[3]<len(history[2])-1 and re.search("NNP?", history[2][history[3]+1][1])) and tag=="PERSON":
+			return 1.0
+		return 0.0
 
-	def is_stopword(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(sentence[index][0] in stopwords.words('english'))
-
-	def is_stemmed(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		stemmer = SnowballStemmer("english")
-		return int(stemmer.stem(sentence[index][0]) == sentence[index][0] )
-
-	def is_number(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(sentence[index][0].replace(".", "").replace(",", "").isdigit())
-
-	def is_abbrevation(history, tag):
-		tag_minus_2, tag_minus_1, sentence, index = history
-		return int(sentence[index][0].replace(".", "").isupper())
+	def isLocation(history, tag):
+		if (history[3]>1 and re.search("NNP?", history[2][history[3]-2][1]) and history[2][history[3]-1][0].strip()==",") or (history[3]<len(history[2])-2 and re.search("NNP?", history[2][history[3]+2][1]) and history[2][history[3]+1][0].strip()==",") and tag=="LOCATION":
+			return 1.0
+		return 0.0
 
 	def is_country(history, tag):
 		tag_minus_2, tag_minus_1, sentence, index = history
 		countries = ["Afghanistan","Aland Islands","Albania","Algeria","American Samoa","AndorrA","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Congo, The Democratic Republic of the","Cook Islands","Costa Rica","Cote D'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands (Malvinas)","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and Mcdonald Islands","Holy See (Vatican City State)","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran, Islamic Republic Of","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea, Democratic People'S Republic of","Korea, Republic of","Kuwait","Kyrgyzstan","Lao People'S Democratic Republic","Latvia","Lebanon","Lesotho","Liberia","Libyan Arab Jamahiriya","Liechtenstein","Lithuania","Luxembourg","Macao","Macedonia, The Former Yugoslav Republic of","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia, Federated States of","Moldova, Republic of","Monaco","Mongolia","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","Northern Mariana Islands","Norway","Oman","Pakistan","Palau","Palestinian Territory, Occupied","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russian Federation","RWANDA","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden","Switzerland","Syrian Arab Republic","Taiwan, Province of China","Tajikistan","Tanzania, United Republic of","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Vanuatu","Venezuela","Viet Nam","Virgin Islands, British","Virgin Islands, U.S.","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe"]
-		return int(sentence[index][0].lower() in [country.lower() for country in countries])
+		return float(sentence[index][0].lower() in [country.lower() for country in countries] and (tag=="LOCATION" or tag=="GPE"))
 
+	def capital_first_letter(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return float(sentence[index][0].isupper() and (tag=="ORGANIZATION" or tag=="LOCATION" or tag=="PERSON" or tag=="GPE" or tag=="FACILITY"))
 
+	# def is_first_word(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(index == 0)
+
+	# def is_last_word(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(index + 1 == len(sentence))
+
+	# def follows_noun(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(tag_minus_1[0] == 'N')
+
+	# def is_stopword(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(sentence[index][0] in stopwords.words('english'))
+
+	# def is_stemmed(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	stemmer = SnowballStemmer("english")
+	# 	return float(stemmer.stem(sentence[index][0]) == sentence[index][0] )
+
+	# def is_number(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(sentence[index][0].replace(".", "").replace(",", "").replace("/", "").replace("-", "").replace("%", "").isdigit())
+
+	# def is_abbrevation(history, tag):
+	# 	tag_minus_2, tag_minus_1, sentence, index = history
+	# 	return float(sentence[index][0].replace(".", "").isupper())
 
 	return [
-		is_first_word,
-		is_last_word,
+		date1,
+		time1,
+		isOther,
+		# is_first_word,
+		# is_last_word,
 		capital_first_letter,
-		follows_noun,
-		is_stopword,
-		is_stemmed,
-		is_number,
-		is_abbrevation,
-		is_country
+		# follows_noun,
+		# is_stopword,
+		# is_stemmed,
+		# is_number,
+		# is_abbrevation,
+		is_country,
+		isLocation,
+		isPerson
 	]
 
 def nltk_tree_to_list(node):
