01d36382004c81d8a3177295e9defdec5b44c376
DruidicHeretic <jetleerd@gmail.com>
2015-09-23 19:12:07 +0530

    Most Recent

--- lab2/code/MyMaxEnt.py
+++ lab2/code/MyMaxEnt.py
@@ -6,15 +6,15 @@ class MyMaxEnt():
 		self.model=np.array([0.0]*len(funcs))
 		self.histLst=histLst
 		self.funcs=funcs
-		self.Y=[]	# IMPORTANT Fill This Up With All Possible Tags. IMPORTANT
+		self.Y=["OTHER","PERSON","GPE","ORGANIZATION","LOCATION","TIME","DATE","MONEY"]	# IMPORTANT Fill This Up With All Possible Tags. IMPORTANT
 		self.trainEx=[]
 		self.testEx=[]
 		# Above Line To Be Used If Both Train And Test Are In The Same File.
 		self.total=0.0
 		self.currentHist={}
 	def create_dataset(self):	# Dataset In The Form Of List Of Individual Examples, Each Of Which Is A Dict, With Keys Being The Possible Tags, And Values The f-vectors Of That Example. One Special Key Is "_exOP", Whose Value Is The Actual Tag Of The Example.
-		self.testList=self.histLst[int(0.8*len(histLst)):]
-		self.histLst=self.histLst[:int(0.8*len(histLst))]
+		self.testList=self.histLst[int(0.8*len(self.histLst)):]
+		self.histLst=self.histLst[:int(0.8*len(self.histLst))]
 		# Above 2 Lines To Be Used If Both Train And Test Are In The Same File.
 		for it in self.histLst:
 			hst=list(it)
@@ -22,13 +22,13 @@ class MyMaxEnt():
 			for jt in self.Y:
 				self.trainEx[-1][jt]=[]
 				for kt in self.funcs:
-					self.trainEx[-1][jt].append(kt(hst[:4],jt))
-			self.trainEx[-1]["_exOP"]=hst[4]
+					self.trainEx[-1][jt].append(kt(hst[0],jt))
+			self.trainEx[-1]["_exOP"]=hst[1]
 	def cost(self,model):
 		self.model=model
 		totalCost=0.0
 		for i in range(len(self.trainEx)):
-			totalCost+=dotP(self.trainEx[i][self.trainEx["_exOP"]])
+			totalCost+=self.dotP(self.trainEx[i][self.trainEx[i]["_exOP"]])
 			nF=0.0
 			for k, v in self.trainEx[i].items():
 				if k!="_exOP":
@@ -51,31 +51,31 @@ class MyMaxEnt():
 	def dotP(self,h):
 		cumul=0.0
 		for ln in range(len(self.model)):
-			self.cumul+=self.model[ln]*h[ln]
+			cumul+=self.model[ln]*h[ln]
 		return cumul
 	def setTot(self):
 		self.total=0.0
-		for v in self.currentHist.values:
-			self.total+=math.exp(dotP(v))
+		for v in self.currentHist.values():
+			self.total+=math.exp(self.dotP(v))
 	def p_y_given_x(self,h,tag):
 		return math.exp(self.dotP(self.currentHist[tag]))/self.total
 	def train(self):	# Call To Train Machine
 		self.create_dataset()
-		params=myMin(self.cost,self.model,method="L-BFGS-B", jac=gradient)	# Add	jac=gradient	As A Parameter For The Optional Part.
+		params=myMin(self.cost,self.model,method="L-BFGS-B")	# Add	jac=gradient	As A Parameter For The Optional Part.
 		self.model=params.x
 		fp=open("maxEntModel.csv","w")
-		fp.write(",".join(list(self.model)))
+		fp.write(",".join([str(itr) for itr in list(self.model)]))
 		fp.close()
 	def gradient(self,model):
 		self.model=model
 		totalCost=0.0
 		for i in range(len(self.trainEx)):
-			totalCost+=self.trainEx[i][self.trainEx["_exOP"]]
+			totalCost+=self.trainEx[i][self.trainEx[i]["_exOP"]]
 			nF=0.0
 			tmpTot=0.0
 			for k, v in self.trainEx[i].items():
 				if k!="_exOP":
-					tmp=math.exp(dotP(v))
+					tmp=math.exp(self.dotP(v))
 					tmpTot+=tmp
 					nF+=v*tmp
 			totalCost-=(float(nF)/tmpTot)
--- lab2/code/tree_parse.py
+++ lab2/code/tree_parse.py
@@ -1,6 +1,62 @@
 import nltk 
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+import MyMaxEnt
 with open('../NDTV_mobile_reviews_Classified/karbonn-kt-21/karbonn-kt-21-review.txt', 'r') as f:
     sample = f.read()
+def get_functions():
+
+	def is_first_word(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(index == 0)
+
+	def is_last_word(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(index + 1 == len(sentence))
+
+	def capital_first_letter(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(sentence[index][0].isupper())
+
+	def follows_noun(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(tag_minus_1[0] == 'N')
+
+	def is_stopword(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(sentence[index][0] in stopwords.words('english'))
+
+	def is_stemmed(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		stemmer = SnowballStemmer("english")
+		return int(stemmer.stem(sentence[index][0]) == sentence[index][0] )
+
+	def is_number(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(sentence[index][0].replace(".", "").replace(",", "").isdigit())
+
+	def is_abbrevation(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		return int(sentence[index][0].replace(".", "").isupper())
+
+	def is_country(history, tag):
+		tag_minus_2, tag_minus_1, sentence, index = history
+		countries = ["Afghanistan","Aland Islands","Albania","Algeria","American Samoa","AndorrA","Angola","Anguilla","Antarctica","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Bouvet Island","Brazil","British Indian Ocean Territory","Brunei Darussalam","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","China","Christmas Island","Cocos (Keeling) Islands","Colombia","Comoros","Congo","Congo, The Democratic Republic of the","Cook Islands","Costa Rica","Cote D'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands (Malvinas)","Faroe Islands","Fiji","Finland","France","French Guiana","French Polynesia","French Southern Territories","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea-Bissau","Guyana","Haiti","Heard Island and Mcdonald Islands","Holy See (Vatican City State)","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran, Islamic Republic Of","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Korea, Democratic People'S Republic of","Korea, Republic of","Kuwait","Kyrgyzstan","Lao People'S Democratic Republic","Latvia","Lebanon","Lesotho","Liberia","Libyan Arab Jamahiriya","Liechtenstein","Lithuania","Luxembourg","Macao","Macedonia, The Former Yugoslav Republic of","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia, Federated States of","Moldova, Republic of","Monaco","Mongolia","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","Northern Mariana Islands","Norway","Oman","Pakistan","Palau","Palestinian Territory, Occupied","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russian Federation","RWANDA","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia and Montenegro","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Georgia and the South Sandwich Islands","Spain","Sri Lanka","Sudan","Suriname","Svalbard and Jan Mayen","Swaziland","Sweden","Switzerland","Syrian Arab Republic","Taiwan, Province of China","Tajikistan","Tanzania, United Republic of","Thailand","Timor-Leste","Togo","Tokelau","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","United States Minor Outlying Islands","Uruguay","Uzbekistan","Vanuatu","Venezuela","Viet Nam","Virgin Islands, British","Virgin Islands, U.S.","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe"]
+		return int(sentence[index][0].lower() in [country.lower() for country in countries])
+
+
+
+	return [
+		is_first_word,
+		is_last_word,
+		capital_first_letter,
+		follows_noun,
+		is_stopword,
+		is_stemmed,
+		is_number,
+		is_abbrevation,
+		is_country
+	]
 
 def nltk_tree_to_list(node):
 	if(type(node) is nltk.tree.Tree):
@@ -14,11 +70,55 @@ def nltk_tree_to_list(node):
 	else:
 		return [(node[0], 'OTHER')]
 
+def create_history(sentence):
+	history = [
+		('*', '*', sentence, 0),
+		('*', sentence[0][1], sentence, 1)
+	]
+
+	for i in range(2, len(sentence)):
+		history += [(sentence[i-2][1], sentence[i-1][1], sentence, i)]
+
+	# history = [
+	# 	('*', '*', 0),
+	# 	('*', sentence[0][1], 1)
+	# ]
+
+	# for i in range(2, len(sentence)):
+	# 	history += [(sentence[i-2][1], sentence[i-1][1], i)]
+
+
+	return history[:len(sentence)]
+
 sentences = nltk.sent_tokenize(sample)
 tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
 tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
 chunked_sentences = nltk.ne_chunk_sents(tagged_sentences)
 chunked_sentences = list(chunked_sentences)
-chunked_sentences = reduce(lambda x, y : x + y, [nltk_tree_to_list(child) for child in chunked_sentences])
+chunked_sentences = [nltk_tree_to_list(child) for child in chunked_sentences]
 
-print chunked_sentences
+words = reduce(lambda x, y : x + y, chunked_sentences)
+
+tags = [word[1] for word in words]
+
+tagset = set(tags)
+# print tags
+history = reduce(lambda x, y : x + y, [create_history(sentence) for sentence in chunked_sentences])
+
+
+args = list(zip(history, tags))
+#print args
+
+maxEnt=MyMaxEnt.MyMaxEnt(args, get_functions())
+maxEnt.train()	# To Train
+# fp=open("maxEntModel.csv","r")	# To Use Pickled Model
+# model=[float(it) for it in fp.read().split(",")]
+# maxEnt.setModel(model)
+# fp.close()
+aCount=0.0
+tCount=0.0
+for it in maxEnt.testList:
+	tCount+=1
+	if maxEnt.classify(it[0])==it[1]:
+		aCount+=1
+print "Accuracy Is:",aCount/tCount
